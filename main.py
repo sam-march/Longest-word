27c026a6e18826a000cfa9cfc975c0ebc8a2da90
Sam March <73301871+sam-march@users.noreply.github.com>
2022-04-25 10:35:49 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+
+def largestWord(inputted_string):
+    inputted_string = sorted(inputted_string, key = len)
+    print(inputted_string[-1])
+  
+
+if __name__ == "__main__":
+    inputted_string = input()
+    l = list(inputted_string.split(" "))
+    largestWord(l)
